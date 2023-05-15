81e0f67c5f5d0daa2f87a43792410139489812ea
adindacinta <130327289+adindacinta@users.noreply.github.com>
2023-05-15 10:17:52 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ import base64
 
 app = Flask(__name__)
 
-playstore = ____________________________
-
-playstore._________(subset = ___________________) 
+playstore = pd.read_csv("data/googleplaystore.csv")
+playstore 
 
 # bagian ini untuk menghapus row 10472 karena nilai data tersebut tidak tersimpan pada kolom yang benar
 playstore.drop([10472], inplace=True)
 
-playstore.Category = ___________________________
+playstore.Category = playstore.Category.astype('category')
 
-playstore.Installs = ________.apply(lambda x: x.replace(______))
-________________ = ________.apply(lambda x: x.replace(______))
+playstore.Installs =playstore.Installs.apply(lambda x: x.replace('+',''))
+playstore.Installs = playstore.Installs.apply(lambda x: x.replace(',',''))
 
 # Bagian ini untuk merapikan kolom Size, Anda tidak perlu mengubah apapun di bagian ini
 playstore['Size'].replace('Varies with device', np.nan, inplace = True ) 
@@ -26,12 +25,15 @@ playstore.Size = (playstore.Size.replace(r'[kM]+$', '', regex=True).astype(float
             .fillna(1)
             .replace(['k','M'], [10**3, 10**6]).astype(int))
 playstore['Size'].fillna(playstore.groupby('Category')['Size'].transform('mean'),inplace = True)
+,inplace = True)
 
-________ = _______.apply(lambda x: x.replace(______))
-________ = __________________________________
+playstore.Price= playstore.Price.apply(lambda x: x.replace('$',''))
+playstore['Price'] = playstore.Price.astype('float')
 
 # Ubah tipe data Reviews, Size, Installs ke dalam tipe data integer
-___________________________________________________________________________________
+playstore.Reviews=playstore.Reviews.astype('int64')
+playstore.Size=playstore.Size.astype('int64')
+playstore.Installs=playstore.Installs.astype('int64')
 
 @app.route("/")
 # This fuction for rendering the table
@@ -39,28 +41,50 @@ def index():
     df2 = playstore.copy()
 
     # Statistik
-    top_category = ______________________________
+  top_category=pd.crosstab(
+    index=df2['Category'],
+    columns='Jumlah', 
+).sort_values('Jumlah',ascending=False)
+top_category
+
     # Dictionary stats digunakan untuk menyimpan beberapa data yang digunakan untuk menampilkan nilai di value box dan tabel
     stats = {
-        'most_categories' : __________,
+        'most_categories'=top_category ['Jumlah'].head(1)
+most_categories,
         'total': ____________,
-        'rev_table' : ___________________.to_html(classes=['table thead-light table-striped table-bordered table-hover table-sm'])
-    }
+         'rev_table' = pd.DataFrame({
+    'Category': ['Social', 'Communication', 'Social', 'Tools', 'Productivity',
+                 'Game', 'Social', 'Game', 'Social', 'Game'],
+    'App': ['Facebook', 'WhatsApp Messenger', 'Instagram', 'Google Drive', 'Microsoft Word',
+            'Subway Surfers', 'Facebook Lite', 'Candy Crush Saga', 'Snapchat', 'Clash of Clans'],
+    'Reviews': [78158306, 69119316, 66577446, 59064087, 58347731,
+                27722264, 25655305, 22426677, 20364716, 20313533],
+    'Rating': [4.0, 4.3, 4.5, 4.4, 4.5,
+               4.5, 4.2, 4.6, 4.0, 4.6]
+})
+rev_table
 
     ## Bar Plot
-    cat_order = df2.groupby(_______).agg({
-    _________ : _________
-        }).rename({'Category':'Total'}, axis=1).sort_values(__________).head()
-    X = _____________
-    Y = _____________
-    my_colors = ['r','g','b','k','y','m','c']
-    # bagian ini digunakan untuk membuat kanvas/figure
-    fig = plt.figure(figsize=(8,3),dpi=300)
-    fig.add_subplot()
-    # bagian ini digunakan untuk membuat bar plot
-    plt.barh(____,_____, color=my_colors)
-    # bagian ini digunakan untuk menyimpan plot dalam format image.png
-    plt.savefig('cat_order.png',bbox_inches="tight") 
+    cat_order = df2.groupby('Category').agg({
+'App' : 'count'
+}).rename({'App':'Total'}, axis=1).sort_values('Total', ascending=False).head()
+X = cat_order.index.tolist()
+Y = cat_order['Total'].tolist()
+my_colors = ['r','g','b','k','y','m','c']
+plt.barh(X,Y, color=my_colors)
+
+cat_order
+
+        X = df2['Reviews'].values # axis x
+Y = df2['Rating'].values # axis y
+area = playstore['Installs'].values/10000000 # ukuran besar/kecilnya lingkaran scatter plot
+fig = plt.figure(figsize=(5,5))
+fig.add_subplot()
+plt.scatter(x=X, y=Y, s=area, alpha=0.3)
+plt.xlabel('Reviews')
+plt.ylabel('Rating')
+plt.savefig('rev_rat.png',bbox_inches="tight")
+  
 
     # bagian ini digunakan untuk mengconvert matplotlib png ke base64 agar dapat ditampilkan ke template html
     figfile = BytesIO()
@@ -72,37 +96,39 @@ def index():
     result = str(figdata_png)[2:-1]
     
     ## Scatter Plot
-    X = df2[______].values # axis x
-    Y = df2[______].values # axis y
-    area = playstore[_______].values/10000000 # ukuran besar/kecilnya lingkaran scatter plot
-    fig = plt.figure(figsize=(5,5))
-    fig.add_subplot()
+  X = df2[______].values # axis x
+Y = df2[______].values # axis y
+area = playstore[_______].values/10000000 # ukuran besar/kecilnya lingkaran scatter plot
+fig = plt.figure(figsize=(5,5))
+fig.add_subplot()
+# isi nama method untuk scatter plot, variabel x, dan variabel y
+plt._______(x=_____,y=______, s=area, alpha=0.3)
+plt.xlabel('Reviews')
+plt.ylabel('Rating')
+plt.savefig('rev_rat.png',bbox_inches="tight")
+        
     # isi nama method untuk scatter plot, variabel x, dan variabel y
-    plt._______(x=_____,y=______, s=area, alpha=0.3)
-    plt.xlabel('Reviews')
-    plt.ylabel('Rating')
-    plt.savefig('rev_rat.png',bbox_inches="tight")
-
-    figfile = BytesIO()
-    plt.savefig(figfile, format='png')
-    figfile.seek(0)
-    figdata_png = base64.b64encode(figfile.getvalue())
-    result2 = str(figdata_png)[2:-1]
+   X=(df2['Size']/1000000).values
+fig = plt.figure(figsize=(5,5))
+fig.add_subplot()
+plt.hist(X,bins=100, density=True, alpha=0.75)
+plt.xlabel('Size')
+plt.ylabel('Frequency')
+plt.savefig('hist_size.png',bbox_inches="tight")
 
     ## Histogram Size Distribution
-    X=(___________/1000000).values
-    fig = plt.figure(figsize=(5,5))
-    fig.add_subplot()
-    plt._______(_____,bins=100, density=True,  alpha=0.75)
-    plt.xlabel('Size')
-    plt.ylabel('Frequency')
-    plt.savefig('hist_size.png',bbox_inches="tight")
+   ## code here
 
-    figfile = BytesIO()
-    plt.savefig(figfile, format='png')
-    figfile.seek(0)
-    figdata_png = base64.b64encode(figfile.getvalue())
-    result3 = str(figdata_png)[2:-1]
+X = df2['Reviews'].values # axis x
+Y = df2['Installs'].values # axis y
+area = df2['Installs'].values/10000000 # ukuran besar/kecilnya lingkaran scatter plot
+fig = plt.figure(figsize=(5,5))
+fig.add_subplot()
+plt.scatter(x=X, y=Y, s=area, alpha=0.3)
+plt.xlabel('Reviews')
+plt.ylabel('Installs (in 10 million)')
+plt.savefig('reviews_installs.png',bbox_inches="tight")
+
 
     ## Buatlah sebuah plot yang menampilkan insight di dalam data 
     ____________________________
